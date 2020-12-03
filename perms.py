7f476884ab69b3ac17a49dffa14cadc8fbd7726a
Alexander Weber <awalexweber99@gmail.com>
2020-12-02 18:07:35 -0800

    Final Code Commit

--- perms.py
+++ perms.py
@@ -1,20 +1,36 @@
+from collections import defaultdict
+total_count = defaultdict(lambda: 0)
 
 cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+cardsStr = ['2','3','4','5','6','7','8','9','10','J','Q','K','A','1']
 scores = [2,3,4,5,6,7,8,9,10,10,10,10,1]
+scoreMap = [2,3,4,5,6,7,8,9,10,10,10,10,1,1]
 
 def calc_prob(score_list):
     # Calculating the estimated next-hand probability
-    
+    total_score = 0
+    total_count = 4 * len(scores)
+    for score in scores:
+        total_score += (score * 4)
+
+    for score in score_list:
+        index = cardsStr.index(str(score))
+        total_score -= scoreMap[index]
+        total_count -= 1
+
+    return total_score / total_count
+
+
 
 def permutate_prob(current_score, score_list, cmap=None):
-    if current_score >= target_score:
+    if calc_prob(score_list) + current_score > 23:
         # Checking for bust, if we bust but we have an ace we reduce the ace to a 1 and continue
         if 'A' in score_list:
             a_index = score_list.index('A')
             score_list[a_index] = 1
             current_score -= 10
 
-        if current_score >= target_score:
+        if calc_prob(score_list) + current_score > 23:
             total_count[current_score] += 1
             return  
 
@@ -31,10 +47,20 @@ def permutate_prob(current_score, score_list, cmap=None):
             new_score_list = score_list.copy()
             new_score_list.append(c)
             if type(c) == int:
-                permutate(current_score + c, new_score_list, new_map)
+                permutate_prob(current_score + c, new_score_list, new_map)
             else:
                 if c == 'J' or c == 'Q' or c == 'K':
-                    permutate(current_score + 10, new_score_list, new_map)
+                    permutate_prob(current_score + 10, new_score_list, new_map)
 
                 elif c == 'A':
-                    permutate(current_score + 11, new_score_list, new_map)
+                    permutate_prob(current_score + 11, new_score_list, new_map)
+
+
+total_count = defaultdict(lambda: 0)
+cmap = defaultdict(lambda: 0)
+print(cmap)
+permutate_prob(0, list(), cmap)
+print("")
+for k in sorted(total_count.keys()):
+    print(k,total_count[k])
+print('')
--- stop_draw.py
+++ stop_draw.py
@@ -14,20 +14,30 @@ class Deck:
         self.totalDrawn = 0
         self.drawnMap = defaultdict(lambda: 0)
 
-        # Building the deck
-        self.deck = list()
-        for card in self.cards:
-            for i in range(4 * size):
-                self.deck.append(card)
+        # Building the deck if our size is nonzero
+        if self.size != 0:
+            self.deck = list()
+            for card in self.cards:
+                for i in range(4 * size):
+                    self.deck.append(card)
 
     def draw(self):
-        card_index = random.randint(0, len(self.deck))
+        if self.size == 0:
+            return random.choice(self.cards)
+
+        if len(self.deck) <= 0:
+            return None
+
+        card_index = random.randint(0, len(self.deck) - 1)
         self.totalDrawn += 1
         card = self.deck[card_index]
         self.drawnMap[card] += 1
         return self.deck.pop(card_index)
 
     def count_prob(self, cardType):
+        if self.size == 0:
+            return 1/13
+
         # Calculating the probability of drawing this card type
         totalCards = (self.size * 4)
         totalCards -= self.drawnMap[cardType]
@@ -35,16 +45,99 @@ class Deck:
         return totalCards / (len(self.deck))
 
     def expectedDrawValue(self):
+        if self.size == 0:
+            o = 0
+            for v in self.values:
+                o += v
+            return o / len(self.values)
+
         o = 0
         for i in range(len(self.cards)):
             o += (self.count_prob(self.cards[i]) * self.values[i])
         return o
 
-d = Deck(1)
-print(d.expectedDrawValue())
-for c in cards:
-    print(d.count_prob(c))
-print(d.draw())
-for c in cards:
-    print(d.count_prob(c))
-print(d.expectedDrawValue())
+target_player_score_set = [15,16,17,18,19,20,21,22,23,24,25]
+
+for player_target_score in target_player_score_set:
+
+    final_scores = {'dealer': 0, 'player': 0, 'tie': 0}
+
+    for iteration in range(0,50000):
+
+        # Creating the deck
+        d = Deck(1)
+
+        # Dealer drawing initial card
+        dealer_card = [d.draw()]
+
+        card_index = d.cards.index(dealer_card[0])
+        dealer_score = d.values[card_index]
+
+        # Drawing two card's for the player
+        player_cards = [d.draw(), d.draw()]
+
+        #print(d.deck)
+        #print(d.expectedDrawValue())
+
+        # Calculating the score
+        player_score = 0
+        for card in player_cards:
+            card_index = d.cards.index(card)
+            player_score += d.values[card_index]
+
+        # Checking if we busted
+        if player_score > player_target_score:
+            # Do we have an ace?
+            if 'A' in player_cards:
+                a_index = player_cards.index('A')
+                player_cards[a_index] = '1'
+                player_score -= 10
+
+        # We will continue to draw while our expectedDrawValue + our current score is less than or equal to 21
+        while d.expectedDrawValue() + player_score <= player_target_score:
+            new_card = d.draw()
+            player_cards.append(new_card)
+            card_index = d.cards.index(new_card)
+            player_score += d.values[card_index]
+
+            # Checking if we busted
+            if player_score > player_target_score:
+                # Do we have an ace?
+                if 'A' in player_cards:
+                    a_index = player_cards.index('A')
+                    player_cards[a_index] = '1'
+                    player_score -= 10
+
+        # Drawing for the dealer
+        while dealer_score < 19:
+            new_card = d.draw()
+            dealer_card.append(new_card)
+            card_index = d.cards.index(new_card)
+            dealer_score += d.values[card_index]
+
+            # Checking if we busted
+            if dealer_score > 21:
+                # Do we have an ace?
+                if 'A' in dealer_card:
+                    a_index = dealer_card.index('A')
+                    dealer_card[a_index] = '1'
+                    dealer_score -= 10
+
+        #print(player_cards,player_score)
+        #print(dealer_card,dealer_score)
+        if player_score > 21:
+            final_scores['dealer'] += 1
+            
+        elif dealer_score > 21:
+            final_scores['player'] += 1
+
+        elif dealer_score > player_score:
+            final_scores['dealer'] += 1
+
+        elif player_score > dealer_score:
+            final_scores['player'] += 1
+
+        elif player_score == dealer_score:
+            final_scores['tie'] += 1
+
+    print(final_scores['dealer'],final_scores['player'],final_scores['tie'],player_target_score)
